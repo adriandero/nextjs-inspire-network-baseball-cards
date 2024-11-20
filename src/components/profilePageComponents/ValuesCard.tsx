12b76d6aef3526f11cbfd0056399dc9f56e08447
adriandero <adrian-nikola.dero@outlook.com>
2024-11-20 21:24:33 +0100

    Fix moreProfiles shrink/grow & flex-wrap

--- src/components/profilePageComponents/ValuesCard.tsx
+++ src/components/profilePageComponents/ValuesCard.tsx
@@ -5,7 +5,7 @@ import { GoDash } from "react-icons/go";
 
 export default function ValuesCard({ profile }: any): React.JSX.Element {
   return (
-    <div className="w-fit max-w-2xl min-w-96 h-fit border border-light3 rounded-2xl p-8 flex flex-row">
+    <div className="max-w-2xl w-full h-fit border border-light3 rounded-2xl p-8 flex flex-row">
       <CardHeader title="Values:" icon={GoNorthStar} iconStrokeWidth={0.5} />
       <div className="flex flex-wrap pl-8 ">
         {profile.values.map((value: string, index: number) => (
