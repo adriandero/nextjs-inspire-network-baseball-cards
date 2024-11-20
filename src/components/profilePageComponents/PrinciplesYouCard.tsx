d534885de64143ccc92ff64ce6adb0466fa1138c
adriandero <adrian-nikola.dero@outlook.com>
2024-11-20 21:26:48 +0100

    Add PrinciplesYou Archetype Card to profile page

---
+++ src/components/profilePageComponents/PrinciplesYouCard.tsx
@@ -0,0 +1,63 @@
+"use client";
+import Image from "next/image";
+
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radioGroup";
+
+import { GoLightBulb, GoStar } from "react-icons/go"; // Importing some icons for the radio items
+
+import { PiDiamondsFour } from "react-icons/pi";
+
+import widgetimage from "@/../public/principleYouIllustraions/coach.png";
+import { ButtonGroup, ButtonGroupItem } from "@/components/ui/ButtonGroup";
+import React from "react";
+
+type principle = {
+  title: string;
+  description: string;
+};
+
+export default function PrinciplesYouCard({ profile }: any): React.JSX.Element {
+  const [value, setValue] = React.useState<principle>(
+    profile?.principleYouArchetype?.[0] ? profile.principleYouArchetype[0] : {}
+  );
+
+  return (
+    <div className="max-w-2xl w-full h-fit border border-light3 rounded-2xl flex flex-col p-8 mt-6">
+      <div className="flex flex-row w-full">
+        <div className="flex flex-row items-center w-fit h-fit">
+          <PiDiamondsFour
+            strokeWidth={0.5}
+            size={24}
+            className="flex self-start"
+          />{" "}
+          <h1 className="text-xl font-bold flex-grow w-fit ml-6">
+            PrinciplesYou Archetype's
+          </h1>
+        </div>
+      </div>
+      <div className="flex flex-row mt-4 gap-6">
+        <div className="flex flex-row mt-4 gap-6">
+          <RadioGroup
+            value={JSON.stringify(value)}
+            onValueChange={(val) => setValue(JSON.parse(val))}
+          >
+            {profile.principleYouArchetype.map(
+              (principle: principle, index: number) => (
+                <RadioGroupItem key={index} value={JSON.stringify(principle)}>
+                  <Image src={widgetimage} alt={principle.title} />
+                </RadioGroupItem>
+              )
+            )}
+          </RadioGroup>
+        </div>
+        <div>
+          <h3 className="font-bold text-lg mt-2">
+            {value.title}
+            {/*<span className="font-normal text-base"> {value.description}</span>*/}
+          </h3>
+          <p className="font-normal"> {value.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
