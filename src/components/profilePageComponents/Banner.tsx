cb00e0bc22e7d2dddff35d009c011f66e1fba2b4
adriandero <adrian-nikola.dero@outlook.com>
2024-11-21 15:39:22 +0100

    Add Avatar & Company Logo to banner

--- src/components/profilePageComponents/Banner.tsx
+++ src/components/profilePageComponents/Banner.tsx
@@ -1,15 +1,46 @@
+import { urlFor } from "@/sanity/client";
+import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
 import { GoPerson } from "react-icons/go";
+import Image from "next/image";
+
+import widgetimage from "@/../public/widgetIllustrations/WIDGET1.png";
 
 export default function Banner({ profile }: any): React.JSX.Element {
+  console.log(profile.team.company.companyLogo.asset.url);
+  console.log(urlFor(profile.team.company.companyLogo).toString());
   return (
-    <div className="w-full h-48 bg-secondary rounded-2xl flex items-center">
-      <div className="w-32 h-32 rounded-full bg-light3 ml-20 mr-12 flex justify-center items-center">
-        <GoPerson className="text-6xl text-dark3" />
-      </div>
+    <div className="w-full max-h-48 h-full bg-secondary rounded-2xl flex items-center px-20">
+      {profile.profileImage ? (
+        <div className="w-32 h-32 rounded-full mr-12 flex justify-center">
+          <Avatar className="">
+            <AvatarImage
+              src={urlFor(profile.profileImage).toString()}
+              className="rounded-full"
+            />
+            <AvatarFallback></AvatarFallback>
+          </Avatar>
+        </div>
+      ) : (
+        <div className="w-32 h-32 rounded-full bg-light3 mr-12 flex justify-center items-center">
+          <GoPerson className="text-6xl text-dark3" />
+        </div>
+      )}
+
       <div>
-        <h1 className="text-3xl font-bold text-light1">{profile.name}</h1>
+        <h1 className="text-3xl font-bold text-light1">
+          {profile.name.toUpperCase()}
+        </h1>
         <h1 className="text-2xl font-bold text-primary">{profile.jobRole}</h1>
       </div>
+
+      <Image
+        src={profile.team.company.companyLogo.asset.url}
+        layout="intrinsic"
+        width={220}
+        height={220}
+        alt="Company Logo"
+        className="ml-auto"
+      />
     </div>
   );
 }
