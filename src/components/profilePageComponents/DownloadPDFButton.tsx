084055f469fe42d20aed039934c3f3b384ad8ada
adriandero <adrian-nikola.dero@outlook.com>
2024-12-11 19:25:42 +0100

    Fix base url to pdf download call

--- src/components/profilePageComponents/DownloadPDFButton.tsx
+++ src/components/profilePageComponents/DownloadPDFButton.tsx
@@ -12,7 +12,7 @@ export default function DownloadButton({ slug }: { slug: string }) {
     try {
       setLoading(true);
       const pdfBlob = await fetch(
-        `https://nextjs-inspire-network-baseball-cards-m94abrzpk.vercel.app/api/profiles/alison-orr/pdf`
+        `${process.env.BASE_URL}/api/profiles/${slug}/pdf`
       ).then((res) => res.blob());
 
       const blobUrl = URL.createObjectURL(pdfBlob);
