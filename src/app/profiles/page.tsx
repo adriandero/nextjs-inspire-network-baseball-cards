a90a0256efdbc8e2fadc75db0c21f442ceabb068
adriandero <adrian-nikola.dero@outlook.com>
2024-12-11 02:40:42 +0100

    Update dashboard && Add Datatable

--- src/app/profiles/page.tsx
+++ src/app/profiles/page.tsx
@@ -1,6 +1,7 @@
 import { getAllProfilesDashboardRowData } from "@/lib/utils/sanityApi/profileRequests";
-import ProfileTable from "@/components/ProfileTable";
 import { checkIfSession } from "@/lib/utils/sessionCheck";
+import { columns } from "@/components/profilesDataTable/columns";
+import { DataTable } from "@/components/profilesDataTable/data-table";
 
 export default async function ProfilePage(): Promise<JSX.Element> {
   await checkIfSession();
@@ -9,15 +10,8 @@ export default async function ProfilePage(): Promise<JSX.Element> {
 
   return (
     <div className="w-full h-screen max-w-screen-lg ">
-      <main className="flex flex-wrap mt-4 gap-8">
-        <ProfileTable
-          profileArray={profiles}
-          _id={""}
-          _rev={""}
-          _type={""}
-          _createdAt={""}
-          _updatedAt={""}
-        />
+      <main className="flex flex-wrap mt-4 gap-8 justify-center">
+        <DataTable columns={columns} data={profiles} />
       </main>
       <footer className="flex item-center p-8"></footer>
     </div>
---
+++ src/components/profilesDataTable/columns.tsx
@@ -0,0 +1,139 @@
+"use client";
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+import { ArrowUpDown } from "lucide-react";
+import { redirect } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Checkbox } from "@/components/ui/checkbox";
+import { SanityDocument } from "next-sanity";
+// import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
+// import { Skeleton } from "../ui/skeleton";
+
+export const columns: ColumnDef<SanityDocument>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "name",
+    footer: "Name" as const,
+    header: "Name",
+    cell: ({ row }) => {
+      const profileName = row.original.name;
+      const profileSlug = row.original.slug;
+      const profileJobRole = row.original.jobRole;
+      // const profileImageSrc =
+      // row.row.original.profileImage?.asset.url ?? "/defaultAvatar.png";
+      //const isAvatarLoaded = useImageLoadState(profileImageSrc);
+
+      return (
+        <div
+          className="flex flex-row items-center gap-4 cursor-pointer"
+          onClick={() => {
+            return redirect(`/profiles/${profileSlug}`);
+          }}
+        >
+          {/* {<Avatar className="block min-w-[40px]">
+            <AvatarImage
+              src={profileImageSrc}
+              width={40}
+              className="rounded-full min-w-[40px]!"
+            />
+            {!isAvatarLoaded ? (
+              <Skeleton
+                className={`min-h-[40px] min-w-[40px] rounded-full bg-light3`}
+              />
+            ) : null}
+            <AvatarFallback></AvatarFallback>
+          </Avatar>} */}
+          <div>
+            <p className="font-bold text-base">{profileName}</p>
+            <div className="table-cell ">{profileJobRole}</div>
+          </div>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "team.name",
+    footer: "Team" as const,
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Team
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "team.company.name",
+    footer: "Company" as const,
+    header: () => <div className="text-right">Company</div>,
+    cell: ({ row }) => {
+      const companyName = row.original.team.company.name || "Unknown Company";
+
+      return <div className="text-right">{companyName}</div>;
+    },
+  },
+  {
+    id: "actions",
+    footer: "Action" as const,
+    cell: ({ row }) => {
+      const profileSlug = row.original.slug;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onClick={() => {
+                return redirect(`/profiles/${profileSlug}`);
+              }}
+            >
+              View Profile
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
