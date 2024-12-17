8e4cf2f4dc85ffe45e218f13761dc3e3aaaa79a4
adriandero <adrian-nikola.dero@outlook.com>
2024-12-17 21:27:38 +0100

    Add nextconfitts ignore .map files

--- next.config.ts
+++ next.config.ts
@@ -5,5 +5,15 @@ const nextConfig: NextConfig = {
     domains: ["cdn.sanity.io"], // Add Sanity's image CDN domain
   },
 };
+module.exports = {
+  webpack: (config: { module: { rules: { test: RegExp; use: string }[] } }) => {
+    // Exclude .map files from being processed by Webpack
+    config.module.rules.push({
+      test: /\.map$/,
+      use: "ignore-loader",
+    });
 
+    return config;
+  },
+};
 export default nextConfig;
