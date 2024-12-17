ba573e291c694bdfc598d6f2585ec317386a9427
adriandero <adrian-nikola.dero@outlook.com>
2024-12-17 22:00:17 +0100

    use puppeteer executable chrome

--- src/app/api/profiles/[slug]/pdf/route.tsx
+++ src/app/api/profiles/[slug]/pdf/route.tsx
@@ -8,6 +8,7 @@ export async function GET(
 ) {
   // const session = await auth0.getSession();
   const slug = (await context.params).slug;
+  const executablePath = puppeteer.executablePath();
 
   const browser = await puppeteer.launch({
     headless: true, // Make sure it's headless
@@ -15,6 +16,7 @@ export async function GET(
       "--no-sandbox", // Prevent sandbox errors (needed for cloud environments like Vercel)
       "--disable-setuid-sandbox", // Disable sandboxing (another requirement for cloud environments)
     ],
+    executablePath: executablePath,
   });
 
   const page = await browser.newPage();
