33f5e1af838dc992b31c62ae02d2fad5869a8501
Adrian-Nikola De Ro <adrian-nikola.dero@outlook.com>
2025-02-24 18:03:02 +0100

    Fix pdf image

--- src/components/profilePDFComponents/PDFBanner.tsx
+++ src/components/profilePDFComponents/PDFBanner.tsx
@@ -9,6 +9,7 @@ export default function PDFBanner({
   profile,
   className,
 }: SanityDocument): React.JSX.Element {
+  console.log(profile)
   return (
     <div className={`${className} w-full items-center`}>
       <div className="w-24 h-24 rounded-full mr-12 flex justify-center overflow-hidden">
@@ -39,7 +40,7 @@ export default function PDFBanner({
         </h1>
       </div>
 
-      {profile?.team && profile?.team?.company?.companyLogo?.asset.url ? (
+      {profile?.team && profile?.team[0]?.company?.companyLogo?.asset.url ? (
         <Image
           src={profile?.team[0]?.company?.companyLogo?.asset.url}
           width={180}
