8a145ced2b217e0d1bf86f1ffa78623f5a8ada99
adriandero <adrian-nikola.dero@outlook.com>
2025-02-28 23:35:01 +0100

    Try directly optimized profile pdf banner

--- frontend/src/components/profilePDFComponents/PDFBanner.tsx
+++ frontend/src/components/profilePDFComponents/PDFBanner.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { urlFor } from "@/lib/sanity/client";
 import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
 import { SanityDocument } from "next-sanity";
