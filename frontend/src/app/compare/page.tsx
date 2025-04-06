b01d512bb34d3aceec62304b6a235e6c442395c3
Adrian-Nikola De Ro <adrian-nikola.dero@outlook.com>
2025-04-06 21:53:36 +0200

    Create compact profile view

---
+++ frontend/src/app/compare/page.tsx
@@ -0,0 +1,83 @@
+import {
+  getAllTeams,
+  getUserTeams,
+  TeamsFromUser,
+} from "@/lib/utils/sanityApi/profileRequests";
+import { DataTable } from "@/components/profilesDataTable/data-table";
+import NavBar from "@/components/NavBar";
+import { SanityDocument } from "next-sanity";
+
+import { auth0 } from "@/lib/auth0";
+import { getUserData } from "@/lib/utils/sessionCheck";
+import { redirect } from "next/navigation";
+import { teamColumns } from "@/components/profilesDataTable/team-columns";
+import { profileColumns } from "@/components/profilesDataTable/profile-columns";
+import { CompareDataTable } from "@/components/compareDataTable/compare-data-table";
+import { SelectedProfilesTable } from "@/components/compareDataTable/selectedProfilesTable";
+import { CompareWrapper } from "./compareWrapper";
+import { compactProfileColumns } from "@/components/compareDataTable/compact-profile-columns";
+
+export interface Team {
+  name: string;
+  slug: string;
+}
+
+export default async function TeamsPage(): Promise<JSX.Element> {
+  const session = await auth0.getSession();
+
+  if (!session) {
+    redirect("/auth/login");
+  }
+
+  const userData = session?.user;
+
+  const userProfileData = await getUserData(userData);
+
+  //TODO: put these functions ins a service or apirequest class because these call the api baseically
+  async function fillAllUserTeams() {
+    let profilesFromUserTeams: TeamsFromUser = { teams: [] };
+
+    if (userProfileData?.team) {
+      profilesFromUserTeams = await getUserTeams(userProfileData.email);
+    }
+
+    return profilesFromUserTeams;
+  }
+
+  //TODO: put these functions ins a service or apirequest class because these call the api baseically
+
+  async function fillDataTableTeamData() {
+    const emptyData: SanityDocument[] = [];
+    if (userProfileData.permission === "Admin") {
+      return await getAllTeams();
+    }
+    const data = userProfileData?.team
+      ? (await fillAllUserTeams()).teams
+      : emptyData;
+
+    return data;
+  }
+
+  return (
+    <div className="w-full h-screen max-w-screen-lg ">
+      <NavBar
+        userProfileData={userProfileData}
+        _id={""}
+        _rev={""}
+        _type={""}
+        _createdAt={""}
+        _updatedAt={""}
+      />
+
+      <main className="flex flex-row justify-center">
+        <CompareWrapper
+          teamColumns={teamColumns}
+          profileColumns={compactProfileColumns}
+          teamsData={await fillDataTableTeamData()}
+          userProfileData={userProfileData}
+        />
+      </main>
+      <footer className="flex item-center p-8"></footer>
+    </div>
+  );
+}
---
+++ frontend/src/components/compareDataTable/compare-data-table.tsx
@@ -0,0 +1,275 @@
+"use client";
+
+import * as React from "react";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/Breadcrumbs";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { GoInfo } from "react-icons/go";
+import { GoMultiSelect } from "react-icons/go";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/Table";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
+import { GoSearch } from "react-icons/go";
+import { SanityDocument } from "next-sanity";
+import { getProfilesFromUserTeams } from "@/lib/utils/sanityApi/profileRequests";
+
+interface DataTableProps<TData, TValue> {
+  teamColumns: ColumnDef<TData, TValue>[];
+  profileColumns: ColumnDef<TData, TValue>[];
+  teamsData: TData[];
+  userProfileData: SanityDocument;
+}
+
+export function CompareDataTable<TData, TValue>({
+  teamColumns,
+  profileColumns,
+  teamsData,
+  userProfileData,
+}: DataTableProps<TData, TValue>) {
+  const [currentView, setCurrentView] = React.useState<"teams" | "profiles">(
+    "teams"
+  );
+  const [selectedTeam, setSelectedTeam] = React.useState<SanityDocument | null>(
+    null
+  );
+  const [profilesData, setProfilesData] = React.useState<SanityDocument[]>([]);
+
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  const handleTeamSelect = async (team: SanityDocument) => {
+    try {
+      const profiles = await getProfilesFromUserTeams(userProfileData.email, [
+        team.slug,
+      ]);
+      setSelectedTeam(team);
+      setProfilesData(profiles.teamProfiles);
+      setCurrentView("profiles");
+      setSorting([]);
+      setColumnFilters([]);
+      setColumnVisibility({});
+      setRowSelection({});
+    } catch (error) {
+      console.error("Error fetching profiles:", error);
+    }
+  };
+
+  const returnToTeamsView = () => {
+    setCurrentView("teams");
+    setSelectedTeam(null);
+    setSorting([]);
+    setColumnFilters([]);
+    setColumnVisibility({});
+    setRowSelection({});
+  };
+
+  const columns = currentView === "teams" ? teamColumns : profileColumns;
+  const data = currentView === "teams" ? teamsData : profilesData;
+
+  const table = useReactTable({
+    data: data as TData[],
+    columns: columns as ColumnDef<TData, TValue>[],
+    onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  return (
+    <div className="sm:min-w-96  w-3/5 max-w-screen-lg sm:px-6 px-2 ">
+      <div className="flex w-full items-center py-4 gap-2">
+        <Breadcrumb className="justify-self-start">
+          <BreadcrumbList>
+            {currentView === "teams" ? (
+              <BreadcrumbItem>
+                <BreadcrumbLink href="#">All Teams</BreadcrumbLink>
+              </BreadcrumbItem>
+            ) : (
+              <>
+                <BreadcrumbItem>
+                  <BreadcrumbLink
+                    onClick={returnToTeamsView}
+                    className="cursor-pointer"
+                  >
+                    All Teams
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  <BreadcrumbLink>{selectedTeam?.name}</BreadcrumbLink>
+                </BreadcrumbItem>
+              </>
+            )}
+          </BreadcrumbList>
+        </Breadcrumb>
+
+        <div className="relative ml-auto">
+          <GoSearch className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+          <Input
+            placeholder="Search names..."
+            value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+            onChange={(event) =>
+              table.getColumn("name")?.setFilterValue(event.target.value)
+            }
+            className="pl-8 !text-base bg-light1"
+          />
+        </div>
+      </div>
+      <div className="rounded-md border bg-light1">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                  onClick={async () => {
+                    if (currentView === "teams")
+                      await handleTeamSelect(row.original as SanityDocument);
+                  }}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24">
+                  <div className="h-fit flex justify-center gap-2">
+                    No results.
+                    {!data.length ? (
+                      <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                          <GoInfo className="flex self-center cursor-pointer" />
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                          <AlertDialogHeader>
+                            <AlertDialogTitle>
+                              Missing Permissions
+                            </AlertDialogTitle>
+                            <AlertDialogDescription>
+                              You don&apos;t have permissions to view any
+                              profiles at the moment. Ask an administrator for
+                              access.
+                            </AlertDialogDescription>
+                          </AlertDialogHeader>
+                          <AlertDialogFooter>
+                            <AlertDialogCancel>OK</AlertDialogCancel>
+                          </AlertDialogFooter>
+                        </AlertDialogContent>
+                      </AlertDialog>
+                    ) : null}
+                    {/* TODO: on click of info -> You don&apos;t have permissions to view any profiles. Ask an administrator for access. */}
+                  </div>
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
+    </div>
+  );
+}
