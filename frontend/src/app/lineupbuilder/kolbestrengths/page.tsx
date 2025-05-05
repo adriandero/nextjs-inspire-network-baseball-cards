0af5a26b6671eed9c2e0568da49fcf767959ba11
Adrian-Nikola De Ro <adrian-nikola.dero@outlook.com>
2025-05-05 16:49:50 +0200

    Add kolbe strengths lineup comparison

---
+++ frontend/src/app/lineupbuilder/kolbestrengths/page.tsx
@@ -0,0 +1,40 @@
+import NavBar from "@/components/NavBar";
+import KolbeStrengthsTable from "@/components/compare/dataTables/kolbeStrengthsTable";
+import { ProfileComparison } from "@/components/lineupBuilder/profileComparison";
+import { auth0 } from "@/lib/auth0";
+import { getUserData } from "@/lib/utils/sessionCheck";
+import { redirect } from "next/navigation";
+import { Suspense } from "react";
+
+// Main page component with Suspense boundary
+export default async function WorkingGeniusPage(): Promise<JSX.Element> {
+  const session = await auth0.getSession();
+
+  if (!session) {
+    redirect("/auth/login");
+  }
+
+  const userData = session?.user;
+
+  const userProfileData = await getUserData(userData);
+
+  return (
+    <div className="w-full max-w-screen-lg">
+      <NavBar
+        userProfileData={userProfileData}
+        _id={""}
+        _rev={""}
+        _type={""}
+        _createdAt={""}
+        _updatedAt={""}
+      />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProfileComparison
+          TableComponent={KolbeStrengthsTable}
+          tableTitle="Kolbe Strengths"
+          tableSlug="kolbestrengths"
+        />
+      </Suspense>
+    </div>
+  );
+}
