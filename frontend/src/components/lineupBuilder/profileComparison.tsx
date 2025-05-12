6807eb1f1c705379d83576515526b42915b02ce3
Adrian-Nikola De Ro <adrian-nikola.dero@outlook.com>
2025-05-12 18:12:03 +0200

    Add lineup selected profiles to local storage

--- frontend/src/components/lineupBuilder/profileComparison.tsx
+++ frontend/src/components/lineupBuilder/profileComparison.tsx
@@ -13,6 +13,7 @@ import {
   GoDownload,
 } from "react-icons/go";
 import { Loader2 } from "lucide-react";
+import { useToast } from "../ui/use-toast";
 
 interface ProfileTable {
   //TODO: own file
@@ -42,6 +43,7 @@ export function ProfileComparison({
   tableSlug,
   tableProps = {},
 }: ProfileComparisonProps) {
+  const { toast } = useToast();
   const [error, setError] = useState<string | null>(null);
   const searchParams = useSearchParams();
   const groupedProfiles = searchParams.get("groupedProfiles");
@@ -115,6 +117,12 @@ export function ProfileComparison({
 
   const handleCopyURLToClipboard = async () => {
     await navigator.clipboard.writeText(window.location.href);
+    // Show toast notification
+    toast({
+      title: "Link copied!",
+      description: "URL has been copied to clipboard",
+      duration: 3000, // 3 seconds
+    });
   };
 
   const [loading, setLoading] = useState(false);
