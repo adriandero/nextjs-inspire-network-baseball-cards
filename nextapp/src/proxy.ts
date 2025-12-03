ecd91b493e452be9d1aba899584d49af83740924
Adrian-Nikola De Ro <adrian-nikola.dero@outlook.com>
2025-12-03 22:00:44 +0100

    Update next app version

--- nextapp/src/proxy.ts
+++ nextapp/src/proxy.ts
@@ -1,7 +1,8 @@
 import type { NextRequest } from "next/server";
 import { auth0 } from "./lib/auth0";
 
-export async function middleware(request: NextRequest) {
+// Change this line ↓
+export async function proxy(request: NextRequest) {
   try {
     return await auth0.middleware(request);
   } catch (err) {
