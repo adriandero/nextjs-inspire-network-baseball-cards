15cd18f8bb4a5e74f12fb0761dc5437c65d53a69
adriandero <adrian-nikola.dero@outlook.com>
2026-01-11 18:59:06 +0100

    outputFileTracingIncludes chromium

--- nextapp/next.config.ts
+++ nextapp/next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "cdn.sanity.io" }], 
+    remotePatterns: [{ protocol: "https", hostname: "cdn.sanity.io" }],
+  },
+  outputFileTracingIncludes: {
+    "*": ["../node_modules/@sparticuz/chromium/**"],
   },
 };
 export default nextConfig;
