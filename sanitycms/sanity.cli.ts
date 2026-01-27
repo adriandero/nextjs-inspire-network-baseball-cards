14739daf92408ce310aa3bb4785c08d422818e04
adriandero <adrian-nikola.dero@outlook.com>
2026-01-27 22:52:40 +0100

    Fix client sice env variables in client.ts

--- sanitycms/sanity.cli.ts
+++ sanitycms/sanity.cli.ts
@@ -1,13 +1,12 @@
-// sanity.cli.ts
 import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '8h77',
+    projectId: '8hl62j77',
     dataset: 'production',
   },
   deployment: {
     autoUpdates: true,
-    appId: 'glnajjwsnmh6162xwghk4ygi', // Add this
+    appId: 'glnajjwsnmh6162xwghk4ygi',
   },
 })
--- sanitycms/sanity.config.ts
+++ sanitycms/sanity.config.ts
@@ -16,9 +16,6 @@ export default defineConfig({
   dataset: 'production',
 
   plugins: [structureTool({structure}), visionTool()],
-  deployment: {
-    appId: 'glnajjwsnmh6162xwghk4ygi',
-  },
   schema: {
     types: schemaTypes,
   },
