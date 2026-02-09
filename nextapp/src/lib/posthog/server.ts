b2143fa952abaef0fa8f952f8454d6277aaf7788
Adrian-Nikola De Ro <62173065+adriandero@users.noreply.github.com>
2026-02-09 17:49:51 +0100

    71: Add team to user mapping for metric leadboard (#72)

--- nextapp/src/lib/posthog/server.ts
+++ nextapp/src/lib/posthog/server.ts
@@ -6,7 +6,7 @@ export function getPostHogClient(): PostHog {
   if (!posthogClient) {
     posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-      disabled: process.env.NEXT_PUBLIC_ENVIRONMENT === "development",
+      // disabled: process.env.NEXT_PUBLIC_ENVIRONMENT === "development",
     });
   }
   return posthogClient;
